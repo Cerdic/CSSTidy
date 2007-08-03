78c161eeb85f456f7c64f2f43332570770a0711f
brettz9 <brettz9@9036ea34-7506-0410-aa24-c7077ae73b50>
2007-08-03 20:17:30 +0000

    Reconciled my copyright change with Sined_'s optimizations/magic quotes fix

--- css_optimiser.php
+++ css_optimiser.php
@@ -1,6 +1,34 @@
 <?php
 
+/**
+ * CSSTidy - CSS Optimiser Interface
+ * This file produces an XHTML interface for optimising CSS code
+ *
+ * Copyright 2005, 2006, 2007 Florian Schmitz
+ *
+ * This file is part of CSSTidy.
+ *
+ *  CSSTidy is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation; either version 2.1 of the License, or
+ *   (at your option) any later version.
+ *
+ *   CSSTidy is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://opensource.org/licenses/lgpl-license.php GNU Lesser General Public License
+ * @package csstidy
+ * @author Florian Schmitz (floele at gmail dot com) 2005-2007
+ * @author Brett Zamir (brettz9 at yahoo dot com) 2007
+ */
+
 header('Content-Type: text/html; charset=utf-8');
+
 require('class.csstidy.php');
 require('lang.inc.php');
 
