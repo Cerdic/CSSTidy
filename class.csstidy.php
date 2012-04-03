d6bfce3f63267b56c82e414331d0be16b7189895
Christopher Finke <cfinke@gmail.com>
2012-04-03 16:14:59 +0000

    Fix for decimal values in @media selectors being stripped of their decimal points.

--- class.csstidy.php
+++ class.csstidy.php
@@ -576,8 +576,8 @@ class csstidy {
 						} elseif ($string{$i} === '\\') {
 							$this->at .= $this->_unicode($string, $i);
 						}
-						// fix for complicated media, i.e @media screen and (-webkit-min-device-pixel-ratio:0)
-						elseif (in_array($string{$i}, array('(', ')', ':'))) {
+						// fix for complicated media, i.e @media screen and (-webkit-min-device-pixel-ratio:1.5)
+						elseif (in_array($string{$i}, array('(', ')', ':', '.'))) {
 							$this->at .= $string{$i};
 						}
 					} else {
