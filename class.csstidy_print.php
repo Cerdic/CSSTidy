929b288996e9eecc89af55ea161e1db285e22d9f
Kimberly <amiga-500@users.noreply.github.com>
2017-01-06 07:56:54 -0500

    @import bug fix
    fixes a bug for @import ".." instead of the expected @import url("..").  If it comes in due to @import ".." the "" will be missing and the output will become @import .. (which is an error) -- see issue 44 for more info

--- class.csstidy_print.php
+++ class.csstidy_print.php
@@ -200,6 +200,12 @@ class csstidy_print {
 					$this->import[$i] = '"' . substr($this->import[$i], 4, -1) . '"';
 					$this->parser->log('Optimised @import : Removed "url("', 'Information');
 				}
+				else if (!preg_match('/^".+"$/',$this->import[$i])) {
+					// fixes a bug for @import ".." instead of the expected @import url("..")
+					// If it comes in due to @import ".." the "" will be missing and the output will become @import .. (which is an error)
+					$this->import[$i] = '"' . $this->import[$i] . '"';
+				}
+
 				$output .= $template[0] . '@import ' . $template[5] . $this->import[$i] . $template[6] . $template[13];
 			}
 		}
